8c7bdedb4ece2de9f8750db35c5bc440a9616b43
Destan <destantezel@googlemail.com>
2023-07-26 02:39:54 +0300

    Make seasons model and register in admin panel

--- seasons/admin.py
+++ seasons/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Seasons
 
-# Register your models here.
+
+@admin.register(Seasons)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ("title", "creator", "start_date")
+    prepopulated_fields = {"slug": ("title",)}
--- seasons/models.py
+++ seasons/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
-# Create your models here.
+
+class Seasons(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    creator = models.ForeignKey(User, on_delete=models.PROTECT)
+    start_date = models.DateField(default=date.today)
+
+    WEEKS_PER_USER = [
+        ("1", "1 Week"),
+        ("2", "2 Weeks"),
+        ("3", "3 Weeks"),
+    ]
+    weeks_per_user = models.CharField(max_length=1, choices=WEEKS_PER_USER, default=2)
+
+    # end_date = groups.users*weeks_per_user  # NOTES: ADD END DATE FUNCTION
