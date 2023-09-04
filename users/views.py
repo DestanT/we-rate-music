6f252fed7d3ba17f1be3caa5868347173ad34e5b
Destan <destantezel@googlemail.com>
2023-09-04 19:25:15 +0100

    Return view and add url pattern for user profiles

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_profile_view(request):
+    return render(request, "users/profile.html")
--- we_rate_music/urls.py
+++ we_rate_music/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("profile/", views.get_profile_view, name="get_profile_view"),
 ]
