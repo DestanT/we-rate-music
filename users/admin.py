3c153fd7f7cd7ab02a90d1acc5258d9b2cd72f78
Destan <destantezel@googlemail.com>
2023-09-07 17:26:18 +0100

    Make models for playlists and songs, migrate, and register in admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import AlbumPlaylist, Songs
 
-# Register your models here.
+
+@admin.register(AlbumPlaylist)
+class AlbumPlaylistAdmin(admin.ModelAdmin):
+    list_display = ("created_by", "title", "created_on")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Songs)
+class SongsAdmin(admin.ModelAdmin):
+    list_display = ("name", "artist", "album_playlist")
