bb8ffe1548ea58dd03b578ff74039571c11501bd
Destan <destantezel@googlemail.com>
2023-09-11 17:05:16 +0100

    Register models in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Playlist, Songs
 
-# from .models import
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("playlist_title",)}
+    list_display = ("user", "playlist_title", "updated_on")
+
+
+@admin.register(Songs)
+class SongsAdmin(admin.ModelAdmin):
+    list_display = ("song_title", "playlist")
