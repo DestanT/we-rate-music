736b32b2e5f21728d50be8f422a3b81be5d04d48
Destan <destantezel@googlemail.com>
2023-09-20 20:13:49 +0100

    Add url path to /add-playlists

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
         views.PlaylistView.as_view(),
         name="user_playlists",
     ),
+    path(
+        "<str:username>/add-playlists",
+        views.CreatePlaylistsView.as_view(),
+        name="add_playlists",
+    ),
     path("<str:username>/clubs", views.ClubView.as_view(), name="user_clubs"),
     path("<str:username>/settings", views.SettingsView.as_view(), name="user_settings"),
 ]
