a3e6007fb59b550801a7c6b0312a8f0736db60f6
Destan <destantezel@googlemail.com>
2023-09-28 22:03:22 +0100

    Delete unused forms

--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,5 @@
-from .models import UserProfile, Playlist, Songs
+from .models import UserProfile
 from django import forms
-
-
-class PlaylistForm(forms.ModelForm):
-    class Meta:
-        model = Playlist
-        fields = ("playlist_name",)
-
-
-class SongsForm(forms.ModelForm):
-    class Meta:
-        model = Songs
-        fields = ("track_name",)
 
 
 class UserProfileImagesForm(forms.ModelForm):
