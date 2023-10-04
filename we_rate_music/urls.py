087c4ce40404b57324920d2fcfcbd72fc5076354
Destan <destantezel@googlemail.com>
2023-10-04 19:07:13 +0100

    Remove unnecessary name attribute from 'profile/' path

--- we_rate_music/urls.py
+++ we_rate_music/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import HomepageView
+from users.views import HomepageView, CustomLoginView
 
 urlpatterns = [
     path("", HomepageView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("profile/", include("users.urls"), name="users_urls"),
+    path("profile/", include("users.urls")),
 ]
