051381d892e173db458b8b89bb64d302216c4fe6
Destan <destantezel@googlemail.com>
2023-10-04 19:19:24 +0100

    Remove unnecessary import CustomLoginView

--- we_rate_music/urls.py
+++ we_rate_music/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import HomepageView, CustomLoginView
+from users.views import HomepageView
 
 urlpatterns = [
     path("", HomepageView.as_view(), name="home"),
