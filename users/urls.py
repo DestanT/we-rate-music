9bbe261747cf9d66ee9baabf7f95a1068096c8a3
Destan <destantezel@googlemail.com>
2023-10-05 16:00:22 +0100

    Change /playlists path name to profile_playlists

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("<str:username>/playlists", views.ProfileView.as_view(), name="user_profile"),
+    path("<str:username>/playlists", views.ProfilePlaylistsView.as_view(), name="profile_playlists"),
     path("<str:username>/playlist_<int:playlist_id>", views.PlaylistDetailsView.as_view(), name="playlist_details"),
     path("<str:username>/add-playlists", views.AddPlaylistsView.as_view(), name="add_playlists"),
     path("<str:username>/clubs", views.ClubView.as_view(), name="user_clubs"),
