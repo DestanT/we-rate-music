62519f0edb7124998d813f663f082e733518498a
Destan <destantezel@googlemail.com>
2023-10-05 16:02:59 +0100

    Overhaul views.py to differentiate between currently logged-in user and the user of which profile is being viewed

--- users/views.py
+++ users/views.py
@@ -12,52 +12,70 @@ class HomepageView(generic.TemplateView):
     template_name = "index.html"
 
 
-class ProfileView(View):
+class ProfilePlaylistsView(View):
     def get(self, request, username, *args, **kwargs):
-        user_profile = get_object_or_404(UserProfile, user__username=username)
-        playlists = Playlist.objects.filter(user=user_profile.user)
+        # Get currently logged-in user's details
+        my_profile = get_object_or_404(UserProfile, user=request.user)
+        my_username = my_profile.user.username
+        my_playlists = Playlist.objects.filter(user=request.user)
+        # Get details for username in the dynamic URL
+        viewed_profile = get_object_or_404(UserProfile, user__username=username)
+        viewed_playlists = Playlist.objects.filter(user=viewed_profile.user)
 
         return render(
             request,
             "users/playlists.html",
             {
-                "user_profile": user_profile,
-                "playlists": playlists,
+                "my_profile": my_profile,
+                "my_username": my_username,
+                "my_playlists": my_playlists,
+                "viewed_profile": viewed_profile,
+                "viewed_playlists": viewed_playlists,
             },
         )
     
 
 class PlaylistDetailsView(View):
     def get(self, request, username, playlist_id, *args, **kwargs):
-        user_profile = get_object_or_404(UserProfile, user__username=username)
-        playlist = Playlist.objects.get(user=user_profile.user, id=playlist_id)
-        tracks = Songs.objects.filter(playlist=playlist)
+        # Get currently logged-in user's details
+        my_profile = get_object_or_404(UserProfile, user=request.user)
+        my_username = my_profile.user.username
+        # Get details for username in the dynamic URL
+        viewed_profile = get_object_or_404(UserProfile, user__username=username)
+        viewed_playlists = Playlist.objects.get(user=viewed_profile.user, id=playlist_id)
+        viewed_tracks = Songs.objects.filter(playlist=viewed_playlists)
 
         return render(
             request,
             "users/playlist_details.html",
             {
-                "user_profile": user_profile,
-                "playlist": playlist,
-                "tracks": tracks,
+                "my_profile": my_profile,
+                "my_username": my_username,
+                "viewed_profile": viewed_profile,
+                "viewed_playlists": viewed_playlists,
+                "viewed_tracks": viewed_tracks,
             },
         )
 
 
 class AddPlaylistsView(View):
     def get(self, request, username, *args, **kwargs):
-        user_profile = get_object_or_404(UserProfile, user__username=username)
+        # Get currently logged-in user's details
+        my_profile = get_object_or_404(UserProfile, user=request.user)
+        my_username = my_profile.user.username
+        # Get details for username in the dynamic URL
+        viewed_profile = get_object_or_404(UserProfile, user__username=username)
 
         # If the user has Spotify username saved in DB; display public Spotify playlists
-        if user_profile.spotify_username:
-            cached_data_key = f"spotify_data_{username}"
+        if my_profile.spotify_username:
+            cached_data_key = f"spotify_data_{my_username}"
 
             # Use cached data, if available
             spotify_playlists = cache.get(cached_data_key)
 
             if spotify_playlists is None:
                 access_token = get_access_token()
-                spotify_playlists = get_user_playlists(access_token, user_profile.spotify_username)
+                spotify_playlists = get_user_playlists(access_token, my_profile.spotify_username)
                 
                 # Save Spotify playlists to cache for 5 minutes
                 cache.set(cached_data_key, spotify_playlists, 300)
@@ -66,7 +84,9 @@ class AddPlaylistsView(View):
                 request,
                 "users/add_playlists.html",
                 {
-                    "user_profile": user_profile,
+                    "my_profile": my_profile,
+                    "my_username": my_username,
+                    "viewed_profile": viewed_profile,
                     "spotify_playlists": spotify_playlists,
                     "playlist_added": False,
                 },
@@ -74,19 +94,24 @@ class AddPlaylistsView(View):
         
         # Else displays message for user; prompting to update Spotify username in "Settings"
         else:
-            missing_username = True
             return render(
                 request,
                 "users/add_playlists.html",
                 {
-                    "user_profile": user_profile,
-                    "missing_username": missing_username,
+                    "my_profile": my_profile,
+                    "my_username": my_username,
+                    "viewed_profile": viewed_profile,
+                    "missing_username": True,
                     "playlist_added": False,
                 },
             )
 
     def post(self, request, username, *args, **kwargs):
-        user_profile = get_object_or_404(UserProfile, user__username=username)
+        # Get currently logged-in user's details
+        my_profile = get_object_or_404(UserProfile, user=request.user)
+        my_username = my_profile.user.username
+        # Get details for username in the dynamic URL
+        viewed_profile = get_object_or_404(UserProfile, user__username=username)
 
         # Add "Playlist" object
         playlist_name = request.POST.get("playlist_name")
@@ -120,22 +145,34 @@ class AddPlaylistsView(View):
             request,
             "users/add_playlists.html",
             {
-                "user_profile": user_profile,
+                "my_profile": my_profile,
+                "my_username": my_username,
+                "viewed_profile": viewed_profile,
                 "spotify_playlists": spotify_playlists,
-                "playlist_added": True,
+                "missing_username": False,
+                "playlist_added": False,
             },
         )
 
 
 class ClubView(View):
     def get(self, request, username, *args, **kwargs):
-        user_profile = get_object_or_404(UserProfile, user__username=username)
-        clubs = MembersInClub.objects.filter(member=user_profile.user)
+        # Get currently logged-in user's details
+        my_profile = get_object_or_404(UserProfile, user=request.user)
+        my_username = my_profile.user.username
+        # Get details for username in the dynamic URL
+        viewed_profile = get_object_or_404(UserProfile, user__username=username)
+        viewed_clubs = MembersInClub.objects.filter(member=viewed_profile.user)
 
         return render(
             request,
             "users/clubs.html",
-            {"user_profile": user_profile, "clubs": clubs},
+            {
+                "my_profile": my_profile,
+                "my_username": my_username,
+                "viewed_profile": viewed_profile,
+                "viewed_clubs": viewed_clubs,
+            },
         )
 
 
@@ -152,20 +189,30 @@ class SettingsView(View):
             }
 
     def get(self, request, username, *args, **kwargs):
-        user_profile = get_object_or_404(UserProfile, user__username=username)
-        placeholder_data = self.get_initial_form_data(user_profile)
+        # Get currently logged-in user's details
+        my_profile = get_object_or_404(UserProfile, user=request.user)
+        my_username = my_profile.user.username
+        placeholder_data = self.get_initial_form_data(my_profile)
+        # Get details for username in the dynamic URL
+        viewed_profile = get_object_or_404(UserProfile, user__username=username)
 
         return render(
             request,
             "users/settings.html",
             {
-                "user_profile": user_profile,
+                "my_profile": my_profile,
+                "my_username": my_username,
+                "viewed_profile": viewed_profile,
                 "image_form": UserSettingsForm(initial=placeholder_data),
             },
         )
 
     def post(self, request, username, *args, **kwargs):
-        user_profile = UserProfile.objects.get(user__username=username)
+        # Get currently logged-in user's details
+        my_profile = get_object_or_404(UserProfile, user=request.user)
+        my_username = my_profile.user.username
+        # Get details for username in the dynamic URL
+        viewed_profile = get_object_or_404(UserProfile, user__username=username)
         settings_form = UserSettingsForm(request.POST, request.FILES)
 
         if settings_form.is_valid():
@@ -173,7 +220,7 @@ class SettingsView(View):
             profile_image = settings_form.cleaned_data["profile_image"]
             background_image = settings_form.cleaned_data["background_image"]
 
-            user_profile.spotify_username = spotify_username
+            my_profile.spotify_username = spotify_username
 
             if profile_image:
                 public_id = f"{username}_profile_image"
@@ -182,7 +229,7 @@ class SettingsView(View):
                     public_id=public_id,
                     folder="we-rate-music/profiles",
                 )
-                user_profile.profile_image = uploaded_image["url"]
+                my_profile.profile_image = uploaded_image["url"]
 
             if background_image:
                 public_id = f"{username}_background_image"
@@ -191,17 +238,19 @@ class SettingsView(View):
                     public_id=public_id,
                     folder="we-rate-music/backgrounds",
                 )
-                user_profile.background_image = uploaded_image["url"]
+                my_profile.background_image = uploaded_image["url"]
 
-            user_profile.save()
+            my_profile.save()
 
-        placeholder_data = self.get_initial_form_data(user_profile)
+        placeholder_data = self.get_initial_form_data(my_profile)
 
         return render(
             request,
             "users/settings.html",
             {
-                "user_profile": user_profile,
+                "my_profile": my_profile,
+                "my_username": my_username,
+                "viewed_profile": viewed_profile,
                 "image_form": UserSettingsForm(initial=placeholder_data),
             },
         )
