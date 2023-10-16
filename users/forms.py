0826844882cca4b586ea8e629dedd2db701d42e2
Destan <destantezel@googlemail.com>
2023-10-16 21:24:18 +0100

    Create form for club invitations

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,21 @@
-from .models import UserProfile, Club
-from django import forms
+from django.forms import ModelForm
+from django.core.exceptions import NON_FIELD_ERRORS
+from .models import UserProfile, Club, ClubInvitation
 
 
-class UserSettingsForm(forms.ModelForm):
+class UserSettingsForm(ModelForm):
     class Meta:
         model = UserProfile
         fields = ("spotify_username", "profile_image", "background_image")
 
 
-class ClubForm(forms.ModelForm):
+class ClubForm(ModelForm):
     class Meta:
         model = Club
         fields = ("club_name", "club_image")
+
+
+class ClubInvitationForm(ModelForm):
+    class Meta:
+        model = ClubInvitation
+        fields = ("user",)
